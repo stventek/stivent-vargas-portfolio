96ab21b2898f1d11ade7cbd361dbb8f078cbd8ac
xenizs <xenizs@protonmail.com>
2023-03-14 20:44:57 -0400

    use json data instead of hardcoded data

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,5 +1,6 @@
 import MuiLink from '@mui/material/Link'
 import { Box, Button, Stack, Typography } from "@mui/material"
+import aboutData from '../../../data/about.json'
 
 export default function About(props: any){
     return <>
@@ -9,13 +10,7 @@ export default function About(props: any){
                     About
                 </Typography> 
                 <Typography>
-                    As a skilled software developer, I carry over more than three years of professional experience.
-                    I enjoy learning and thinking outside the box to push things forward. 
-                    I'm all about finding practical and efficient solutions that fit the situation at hand.
-                    I have a passion for developing high-quality software solutions using agile methodologies. 
-                    As a full-stack, I've had the opportunity to take part on a broad range of development projects. 
-                    I'm very dedicated and hardworking, and I'm always looking for ways to make my work better. 
-                    I'm adaptable, I work hard, and I'm all about technology. I always want to make a difference in every project I take on.
+                    {aboutData.description}
                 </Typography>
                 <Button LinkComponent={MuiLink} target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1kGW-bZ-iEaW-b1H7Qeoe8LLz_Kbkud6_ateMPtDstTE" variant="contained">Curriculum </Button>
             </Stack>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,15 @@
 import { Box, Stack, Typography } from "@mui/material";
+import headerData from '../../../data/header.json'
 
 export default function Header(props: any){
     return <>
         <Box component="section" id="home" sx={{scrollMarginTop: (theme) => theme.mixins.toolbar.minHeight + theme.spacing(3)}}>
             <Stack alignItems="center" spacing={1}>
                 <Typography variant="h3" component="h1" align="center">
-                    Hi, I'm Stivent Vargas
+                    {headerData.title}
                 </Typography>
                 <Typography variant="subtitle1" align="center">
-                I'm a Full Stack Software Developer with more than 3 years of professional experience
+                    {headerData.subtitle}
                 </Typography>
             </Stack>
         </Box>
